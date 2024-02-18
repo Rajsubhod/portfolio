46c3f444b3c0fad649cf6a24e5e6bd0a0a1888cf
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-18 22:33:42 +0530

    enhancing its functionality with framer motion

--- components/header.tsx
+++ components/header.tsx
@@ -14,13 +14,10 @@ export const Header = () => {
 				animate={{ y: 0, x: '-50%', opacity: 1 }}
 			></motion.div>
 			<nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0  ">
-				<motion.ul
-					className="flex flex-row flex-wrap w-[22rem] items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5"
-					animate={{ staggerChildren: 0.1 }}
-				>
+				<ul className="flex flex-row flex-wrap w-[22rem] items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5 ">
 					{links.map((link) => (
 						<motion.li
-							className="h-3/4 flex items-center justify-center"
+							className="h-3/4 flex items-center justify-center  "
 							key={link.hash}
 							initial={{ y: -100, opacity: 0 }}
 							animate={{ y: 0, opacity: 1 }}
@@ -33,7 +30,7 @@ export const Header = () => {
 							</Link>
 						</motion.li>
 					))}
-				</motion.ul>
+				</ul>
 			</nav>
 		</header>
 	);
