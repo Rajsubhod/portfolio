65290fdcd0200ca7e150ab69cac590dc61e2e670
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-18 14:10:56 +0530

    adding functionlity to header

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
+import { links } from '@/lib/data';
+import Link from 'next/link';
 
 export const Header = () => {
 	return (
@@ -11,6 +13,28 @@ export const Header = () => {
 				initial={{ y: -100, x: '-50%', opacity: 0 }}
 				animate={{ y: 0, x: '-50%', opacity: 1 }}
 			></motion.div>
+			<nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0  ">
+				<motion.ul
+					className="flex flex-row flex-wrap w-[22rem] items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5"
+					animate={{ staggerChildren: 0.1 }}
+				>
+					{links.map((link) => (
+						<motion.li
+							className="h-3/4 flex items-center justify-center"
+							key={link.hash}
+							initial={{ y: -100, opacity: 0 }}
+							animate={{ y: 0, opacity: 1 }}
+						>
+							<Link
+								className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition ease-in-out duration-300"
+								href={link.hash}
+							>
+								{link.name}
+							</Link>
+						</motion.li>
+					))}
+				</motion.ul>
+			</nav>
 		</header>
 	);
 };
--- lib/data.ts
+++ lib/data.ts
@@ -39,7 +39,7 @@ export const experiencesData = [
 		location: 'Miami, FL',
 		description:
 			'I graduated after 6 months of studying. I immediately found a job as a front-end developer.',
-		// icon: React.createElement(LuGraduationCap),
+		icon: React.createElement(LuGraduationCap),
 		date: '2019',
 	},
 	{
@@ -47,7 +47,7 @@ export const experiencesData = [
 		location: 'Orlando, FL',
 		description:
 			'I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.',
-		// icon: React.createElement(CgWorkAlt),
+		icon: React.createElement(CgWorkAlt),
 		date: '2019 - 2021',
 	},
 	{
@@ -55,7 +55,7 @@ export const experiencesData = [
 		location: 'Houston, TX',
 		description:
 			"I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-		// icon: React.createElement(FaReact),
+		icon: React.createElement(FaReact),
 		date: '2021 - present',
 	},
 ] as const;
