8c0b89662f1058e00673aacab9853bab2f4c4ff1
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-19 23:01:58 +0530

    uodate data

--- lib/data.ts
+++ lib/data.ts
@@ -2,8 +2,8 @@ import React from 'react';
 import { CgWorkAlt } from 'react-icons/cg';
 import { FaReact } from 'react-icons/fa';
 import { LuGraduationCap } from 'react-icons/lu';
-// import corpcommentImg from '@/public/corpcomment.png';
-// import rmtdevImg from '@/public/rmtdev.png';
+import corpcommentImg from '@/public/corpcomment.png';
+import rmtdevImg from '@/public/rmtdev.png';
 // import wordanalyticsImg from '@/public/wordanalytics.png';
 
 export const links = [
@@ -66,23 +66,23 @@ export const projectsData = [
 		description:
 			'I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.',
 		tags: ['React', 'Next.js', 'MongoDB', 'Tailwind', 'Prisma'],
-		// imageUrl: corpcommentImg,
+		imageUrl: corpcommentImg,
 	},
 	{
 		title: 'rmtDev',
 		description:
 			'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
 		tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
-		// imageUrl: rmtdevImg,
+		imageUrl: rmtdevImg,
 	},
 	{
 		title: 'Word Analytics',
 		description:
 			'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
 		tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
-		// imageUrl: wordanalyticsImg,
+		imageUrl: '',
 	},
-] as const;
+];
 
 export const skillsData = [
 	'HTML',
