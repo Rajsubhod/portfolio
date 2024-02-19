2965e0db48536852644ce166f196937109359350
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-19 20:12:32 +0530

    a componenet to esily create section headings

---
+++ components/section-heading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SectionHeading({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return <h2 className="font-medium text-3xl capitalize mb-8">{children}</h2>;
+}
