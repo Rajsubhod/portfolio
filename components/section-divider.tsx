219f884f748da80faa03e4f21b505902325938b3
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-20 21:13:04 +0530

    renaming changes

---
+++ components/section-divider.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+import { motion } from 'framer-motion';
+
+export default function SectionDivider() {
+	return (
+		<motion.div
+			className="bg-gray-300 my-24 h-16 w-1 rounded-full hidden sm:block"
+			initial={{ y: 100, opacity: 0 }}
+			animate={{ y: 0, opacity: 1 }}
+			transition={{ delay: 0.125 }}
+		></motion.div>
+	);
+}
