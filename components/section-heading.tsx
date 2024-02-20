3d7864aaa8d97fb02d07d702e20b8ed98d106e72
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-20 21:25:13 +0530

    Update section-heading component styling

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -6,7 +6,7 @@ export default function SectionHeading({
 	children: React.ReactNode;
 }) {
 	return (
-		<h2 className="font-medium text-3xl capitalize mb-8 text-center">
+		<h2 className="font-medium text-3xl capitalize mb-8 pb-1 text-center">
 			{children}
 		</h2>
 	);
