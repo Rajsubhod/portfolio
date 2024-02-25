64ccf99640c109a221968281be75a86b63e56ebf
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-26 00:24:27 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,10 +22,7 @@ export default function RootLayout({
 				className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}
 			>
 				<div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-				<div
-					className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[60rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]
-        sm:left-[-38rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
-				></div>
+				<div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[60rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]sm:left-[-38rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
 				<Header />
 				{children}
