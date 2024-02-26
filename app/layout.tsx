67c93ce4d0764c0228b24478fbbb0013003b92a3
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-26 23:31:28 +0530

    Add ActiveSectionContextProvider and useActiveSectionContext

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/header';
+import ActiveSectionContextProvider from '@/context/active-section-context';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,8 +25,10 @@ export default function RootLayout({
 				<div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
 				<div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[60rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]sm:left-[-38rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
-				<Header />
-				{children}
+				<ActiveSectionContextProvider>
+					<Header />
+					{children}
+				</ActiveSectionContextProvider>
 			</body>
 		</html>
 	);
--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,5 +1,37 @@
-import React from 'react';
+'use client';
 
-export default function ActiveSectionContextProvider() {
-	return <div>ActiveSectionContextProvider</div>;
+import React, { useState } from 'react';
+import { links } from '@/lib/data';
+
+type SectionName = (typeof links)[number]['name'];
+type ActiveSectionContextType = {
+	activeSection: SectionName;
+	setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+};
+
+export const ActiveSectionContext =
+	React.createContext<ActiveSectionContextType | null>(null);
+
+export default function ActiveSectionContextProvider({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	const [activeSection, setActiveSection] = useState<SectionName>('Home');
+
+	return (
+		<ActiveSectionContext.Provider value={{ activeSection, setActiveSection }}>
+			{children}
+		</ActiveSectionContext.Provider>
+	);
+}
+
+export function useActiveSectionContext() {
+	const context = React.useContext(ActiveSectionContext);
+	if (context === null) {
+		throw new Error(
+			'useActiveSectionContext must be used within a ActiveSectionContextProvider'
+		);
+	}
+	return context;
 }
