04622df7a08b33845dc02d504857460f86d665c1
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-26 23:53:22 +0530

    Add client-side rendering and active section tracking to project list component

--- components/project-list.tsx
+++ components/project-list.tsx
@@ -1,11 +1,26 @@
+'use client';
+
 import React from 'react';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useActiveSectionContext } from '@/context/active-section-context';
 import SectionHeading from './section-heading';
 import { projectsData } from '@/lib/data';
 import Project from '@/components/project';
 
 export default function Projects() {
+	const { ref, inView } = useInView({
+		threshold: 0.5,
+	});
+	const { setActiveSection } = useActiveSectionContext();
+
+	useEffect(() => {
+		if (inView) {
+			setActiveSection('Projects');
+		}
+	}, [inView, setActiveSection]);
 	return (
-		<section id="projects" className="scroll-m-28">
+		<section ref={ref} id="projects" className="scroll-m-28">
 			<SectionHeading>My projects</SectionHeading>
 			<div>
 				{projectsData.map((project, index) => (
