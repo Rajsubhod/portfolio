d8580e8a909ccb3175d20ccefcbd31d946be90de
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-26 23:52:52 +0530

    Add useInView and useEffect hooks to About component

--- components/about.tsx
+++ components/about.tsx
@@ -1,12 +1,26 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import SectionHeading from '@/components/section-heading';
 import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function About() {
+	const { ref, inView } = useInView({
+		threshold: 0.75,
+	});
+	const { setActiveSection } = useActiveSectionContext();
+
+	useEffect(() => {
+		if (inView) {
+			setActiveSection('About');
+		}
+	}, [inView, setActiveSection]);
+
 	return (
 		<motion.section
+			ref={ref}
 			className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-m-28"
 			id="about"
 			initial={{ opacity: 0, y: 100 }}
