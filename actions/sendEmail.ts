2b9c2626ba2df76295b3ac924ad2aedbef259b6d
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-28 21:02:09 +0530

    refactor form to use formik and yup validation library

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,32 +1,18 @@
 'use server';
 
-import { z } from 'zod';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const schema = z.object({
-	email: z
-		.string({
-			required_error: 'Please enter your email',
-			invalid_type_error: 'Please enter a valid email',
-		})
-		.trim()
-		.email(),
-	message: z
-		.string({
-			required_error: 'Message cannot be empty',
-		})
-		.max(255, {
-			message: 'Message cannot be more than 255 characters',
-		}),
-});
-
-export const sendEmail = async (formData: FormData) => {
-	const { email, message } = {
-		email: formData.get('senderEmail'),
-		message: formData.get('message'),
+type dataType = {
+	values: {
+		senderEmail: string;
+		message: string;
 	};
+};
+
+export const sendEmail = async (formData: dataType) => {
+	const { senderEmail: email, message } = formData.values;
 
 	if (
 		!email ||
@@ -42,41 +28,17 @@ export const sendEmail = async (formData: FormData) => {
 		};
 	}
 
-	const validatedFields = schema.safeParse({ email, message });
-
-	if (!validatedFields.success) {
-		console.log(validatedFields.error.flatten().fieldErrors);
-		return {
-			errors: validatedFields.error.flatten().fieldErrors,
-		};
-	}
-
 	const { data, error } = await resend.emails.send({
-		from: 'rejeo.wiki <onboarding@resend.dev>',
-		to: ['rajdeepmukherjee2002@gmail.com'],
+		from: 'www.rejeo.wiki <onboarding@resend.dev>',
+		to: ['rajdeepmukherjee721@gmail.com'],
 		subject: 'Hello, World!',
 		text: message,
 		reply_to: email,
 	});
 
 	if (error) {
-		return {
-			errors: error.message,
-		};
+		return error.message;
 	}
+
+	return 'Email sent successfully';
 };
-
-// export default async () => {
-// 	const { data, error } = await resend.emails.send({
-// 		from: 'Acme <onboarding@resend.dev>',
-// 		to: ['delivered@resend.dev'],
-// 		subject: 'Hello, World!',
-// 		react: EmailTemplate({ firstName: 'John' }),
-// 	});
-
-// 	if (error) {
-// 		return res.status(400).json(error);
-// 	}
-
-// 	res.status(200).json(data);
-// };
