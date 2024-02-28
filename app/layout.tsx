16177bdc9896288897ec1914c35dea614bef64b5
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-02-28 17:42:48 +0530

    Added footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/header';
 import ActiveSectionContextProvider from '@/context/active-section-context';
+import Footer from '@/components/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
 				<ActiveSectionContextProvider>
 					<Header />
 					{children}
+					<Footer />
 				</ActiveSectionContextProvider>
 			</body>
 		</html>
