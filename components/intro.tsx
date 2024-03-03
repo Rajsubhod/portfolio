59381231f6a2d454f7a8ccbea56454c556e0246a
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-03-03 20:56:47 +0530

    added social buttons in intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,10 +5,11 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
 import Rajdeep from '@/public/Rajdeep.jpeg';
-import { BsArrowRight } from 'react-icons/bs';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
 import { useSectionInView } from '@/lib/hooks';
 import { useActiveSectionContext } from '@/context/active-section-context';
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
 	const { ref } = useSectionInView({ sectionName: 'Home', threshold: 0.5 });
@@ -17,7 +18,7 @@ export default function Intro() {
 	return (
 		<section
 			ref={ref}
-			className="mb-28 max-w-[40rem] text-center sm:mb-0 select-none scroll-m-[100rem]"
+			className="mb-28 max-w-[42rem] text-center sm:mb-0 select-none scroll-m-[100rem]"
 			id="home"
 		>
 			<div className="flex items-center justify-center">
@@ -67,7 +68,7 @@ export default function Intro() {
 			</motion.h1>
 
 			<motion.div
-				className="flex gap-2 items-center justify-center flex-col sm:flex-row px-4 text-lg font-medium"
+				className="flex gap-2 items-center justify-center flex-col sm:flex-row px-4 text-lg font-medium w-full"
 				initial={{ opacity: 0, y: 100 }}
 				animate={{ opacity: 1, y: 0 }}
 				transition={{ delay: 0.1 }}
@@ -93,7 +94,7 @@ export default function Intro() {
 				</Link>
 
 				<a
-					className="bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full sm:hover:scale-110 hover:bg-gray-300	sm:hover:translate-x-[1rem] active:scale-90 sm:active:scale-105 sm:active:translate-x-[0.5rem] transition group cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white dark:hover:bg-white/30 "
+					className="bg-white text-black px-[1.50rem] py-3 flex items-center gap-2 rounded-full sm:hover:scale-110 hover:bg-gray-300	sm:hover:translate-x-[1rem] active:scale-90 sm:active:scale-105 sm:active:translate-x-[0.5rem] transition group cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white dark:hover:bg-white/30 "
 					href="/My Resume.pdf"
 					download={true}
 					draggable={false}
@@ -105,6 +106,24 @@ export default function Intro() {
 					transition"
 					/>
 				</a>
+				<div className="flex justify-center items-center gap-3">
+					<a
+						className="bg-white ml-6 p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition 
+						hover:bg-gray-300 cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/30 "
+						href="https://linkedin.com/in/rajdeép-mukherjee/"
+						target="_blank"
+					>
+						<BsLinkedin />
+					</a>
+
+					<a
+						className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition  hover:bg-gray-300 cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/30 "
+						href="https://github.com/Rajsubhod"
+						target="_blank"
+					>
+						<FaGithubSquare />
+					</a>
+				</div>
 			</motion.div>
 		</section>
 	);
